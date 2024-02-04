670da0df8be283e82a4695a7ff8fa1cc5722ba43
Thisarani kaushalya <117965713+Thisarani123@users.noreply.github.com>
2024-02-04 19:24:42 +0530

    update

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackColor = Color(0xff303030);
+  static const Color greenColor = Color.fromARGB(255, 45, 114, 187);
+  static const Color greyColor = Color.fromARGB(255, 49, 115, 238);
+  static const Color whiteColor = Color.fromARGB(255, 55, 84, 181);
+  static const Color lightColor = Color.fromARGB(255, 158, 205, 247);
+}
