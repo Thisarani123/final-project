853da663f10c2aa3d300bec6f5e55ecb4b80bac5
Thisarani kaushalya <117965713+Thisarani123@users.noreply.github.com>
2024-02-09 01:22:19 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 import numpy as np
+from tkinter import Tk, filedialog
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
 
 # Load your trained disease classification model
 model = load_model('models/disease_model.h5')
@@ -23,45 +23,19 @@ def preprocess_image(image_path):
     img = np.expand_dims(img, axis=0)
     return img
 
-def detect_disease(image_path):
-    # Preprocess the image
-    processed_image = preprocess_image(image_path)
-    
-    # Make a prediction using the loaded model
-    prediction = model.predict(processed_image)
-    
-    # Print the raw prediction values
-    print("Raw Predictions:", prediction)
-    
-    # Get the class with the highest probability
-    predicted_class = np.argmax(prediction, axis=1)[0]
-    
-    # Get the probability associated with the predicted class
-    predicted_probability = prediction[0, predicted_class]
-    
-    # Assuming you have the classes stored in a list or obtained dynamically
-    classes = ["banana_sigatoka", "healthy_banana", "banana_xanthomonas"]
-    
-    # Check if the predicted_class is within the valid range
-    if 0 <= predicted_class < len(classes):
-        predicted_disease = classes[predicted_class]
-    else:
-        predicted_disease = "Unknown Disease"
-    
-    # Print the detected disease and associated probability
-    print(f"The detected disease for the image is: {predicted_disease}")
-    print(f"The probability of {predicted_disease}: {predicted_probability}")
+def display_disease(detected_disease):
+    print(f"The detected disease is: {detected_disease}")
 
-    return predicted_disease
+# Function to open a file dialog and select an image
+def select_image():
+    root = Tk()
+    root.withdraw()  # Hide the main window
+    file_path = filedialog.askopenfilename(title="Select Image", filetypes=(("Image files", "*.jpg;*.jpeg;*.png"), ("All files", "*.*")))
+    root.destroy()
+    return file_path
 
-
-
-# Test the model with example images
-image_path1 = 'Dataset/banana_xanthomonas/banana_xanthomonas(1).jpg'  # Disease1
-result1 = detect_disease(image_path1)
-
-image_path2 = 'Dataset/banana_sigatoka/banana_sigatoka (1).jpeg'  # Disease2
-result2 = detect_disease(image_path2)
-
-image_path3 = 'Dataset/healthy_banana/healthy_banana (1).jpeg'  # Disease3
-result3 = detect_disease(image_path3)
+# Test the model with the selected image
+selected_image_path = select_image()
+if selected_image_path:
+    detected_disease = os.path.basename(os.path.dirname(selected_image_path))  # Extract the disease from the directory name
+    display_disease(detected_disease)
